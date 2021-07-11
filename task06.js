81bb4e6c168d5fdc5a435f325002c67d0fb1391e
Ant0n0vS <successful1234@yandex.ru>
2021-07-12 00:05:16 +0300

    Update and rename task6.js to task06.js

--- task06.js
+++ task06.js
@@ -1,8 +1,5 @@
-/*
-Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string,
-the longest possible, containing distinct letters - each taken only once - coming from 
-s1 or s2.
-*/
+//Two to One (7 kyu)
+//https://www.codewars.com/kata/5656b6906de340bd1b0000ac
 
 function longest(s1, s2) {
     let mainLine = s1 + s2;
