fd0f0e84af5cf9434f5914cfd982d033e5ea38a0
Ant0n0vS <successful1234@yandex.ru>
2021-07-12 00:04:23 +0300

    Update and rename task5.js to task05.js

--- task05.js
+++ task05.js
@@ -1,9 +1,6 @@
-/*
-Welcome. In this kata, you are asked to square every digit of a number 
-and concatenate them.
-For example, if we run 9119 through the function, 811181 will come out, 
-because 9^2 is 81 and 1^2 is 1.
-*/
+//Square Every Digit (7 kyu)
+//https://www.codewars.com/kata/546e2562b03326a88e000020
+
 function squareDigits(num){
     if (num == 0) {
         return 0;
