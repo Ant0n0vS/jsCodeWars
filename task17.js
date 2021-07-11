c7b76780c842888e9e2d5d1cfb8b754bba5e08e3
Ant0n0vS <successful1234@yandex.ru>
2021-07-12 00:13:10 +0300

    Update

--- task17.js
+++ task17.js
@@ -1,13 +1,5 @@
 //Regex Password Validation (5 kyu)
-/*
-You need to write regex that will validate a password to make sure 
-it meets the following criteria:
-    ► At least six characters long
-    ► contains a lowercase letter
-    ► contains an uppercase letter
-    ► contains a number
-Valid passwords will only be alphanumeric characters.
-*/
+//https://www.codewars.com/kata/52e1476c8147a7547a000811
 
 function validate(password) {
     return /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])\w{6,}$/m.test(password);
