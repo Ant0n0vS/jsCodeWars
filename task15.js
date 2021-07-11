2fdf25411d65cd57e6a66e46371c4b32c48b4285
Ant0n0vS <successful1234@yandex.ru>
2021-07-12 00:11:53 +0300

    Update

--- task15.js
+++ task15.js
@@ -1,8 +1,6 @@
 //List Filtering (7 kyu)
-/*
-In this kata you will create a function that takes a list of non-negative
- integers and strings and returns a new list with the strings filtered out.
-*/
+//https://www.codewars.com/kata/53dbd5315a3c69eed20002dd
+
 function filterList(l) {
     for (let i = l.length - 1; i >= 0; i--) {
         if (typeof(l[i]) != 'number') {
